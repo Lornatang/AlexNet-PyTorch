5174de3e899f91261b758afffddba2e05398a71c
Changyu Liu <liuchangyu1111@gmail.com>
2019-12-19 12:30:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ from utils.eval import accuracy
 from utils.misc import AverageMeter
 
 model_urls = {
-    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
+  'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
 }
 
 parser = argparse.ArgumentParser(description='PyTorch AlexNet Classifier')
@@ -61,9 +61,9 @@ parser.add_argument('--manualSeed', type=int, help='manual seed')
 opt = parser.parse_args()
 
 try:
-    os.makedirs(opt.checkpoints_dir)
+  os.makedirs(opt.checkpoints_dir)
 except OSError:
-    pass
+  pass
 
 manualSeed = random.randint(1, 10000)
 random.seed(manualSeed)
@@ -84,18 +84,17 @@ MODEL_PATH = os.path.join(opt.checkpoints_dir, f"{opt.name}.pth")
 
 train_dataset = dset.ImageFolder(root=TRAIN_DATASETS_PATH,
                                  transform=transforms.Compose([
-                                     transforms.Resize((opt.img_size, opt.img_size), interpolation=3),
-                                     transforms.RandomResizedCrop(224),
-                                     transforms.ToTensor(),
-                                     transforms.Normalize([0.485, 0.456, 0.406], [
-                                                          0.229, 0.224, 0.225]),
+                                   transforms.Resize((256, 256), interpolation=3),
+                                   transforms.RandomResizedCrop(opt.img_size),
+                                   transforms.RandomHorizontalFlip(),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
                                  ]))
 test_dataset = dset.ImageFolder(root=TEST_DATASETS_PATH,
                                 transform=transforms.Compose([
-                                    transforms.Resize((opt.img_size, opt.img_size), interpolation=3),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize([0.485, 0.456, 0.406], [
-                                                         0.229, 0.224, 0.225]),
+                                  transforms.Resize((opt.img_size, opt.img_size), interpolation=3),
+                                  transforms.ToTensor(),
+                                  transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
                                 ]))
 
 train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=opt.batch_size,
@@ -106,98 +105,100 @@ test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=opt.batch
 
 
 def train():
-    try:
-        os.makedirs(opt.checkpoints_dir)
-    except OSError:
-        pass
-    if torch.cuda.device_count() > 1:
-      model = torch.nn.parallel.DataParallel(AlexNet(**kwargs))
-    else:
-      model = AlexNet(num_classes=opt.num_classes)
-    if pretrained:
-      state_dict = load_state_dict_from_url(model_urls['alexnet'],
-                                            progress=progress)
-      model.load_state_dict(state_dict)
-    ################################################
-    # Set loss function and Adam optimizer
-    ################################################
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=opt.lr)
+  try:
+    os.makedirs(opt.checkpoints_dir)
+  except OSError:
+    pass
+  if torch.cuda.device_count() > 1:
+    model = torch.nn.parallel.DataParallel(AlexNet(num_classes=opt.num_classes))
+  else:
+    model = AlexNet(num_classes=opt.num_classes)
+  if opt.pretrained:
+    state_dict = load_state_dict_from_url(model_urls['alexnet'], progress=progress)
+    model.load_state_dict(state_dict)
+  model.to(device)
+  ################################################
+  # Set loss function and Adam optimizer
+  ################################################
+  criterion = torch.nn.CrossEntropyLoss()
+  optimizer = optim.Adam(model.parameters(), lr=opt.lr)
 
-    for epoch in range(opt.epochs):
-        # train for one epoch
-        print(f"\nBegin Training Epoch {epoch + 1}")
-        # Calculate and return the top-k accuracy of the model
-        # so that we can track the learning process.
-        losses = AverageMeter()
-        top1 = AverageMeter()
-        top5 = AverageMeter()
+  for epoch in range(opt.epochs):
+    # train for one epoch
+    print(f"\nBegin Training Epoch {epoch + 1}")
+    # Calculate and return the top-k accuracy of the model
+    # so that we can track the learning process.
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
 
-        for i, data in enumerate(train_dataloader):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, targets = data
-            inputs = inputs.to(device)
-            targets = targets.to(device)
+    for i, data in enumerate(train_dataloader):
+      # get the inputs; data is a list of [inputs, labels]
+      inputs, targets = data
+      inputs = inputs.to(device)
+      targets = targets.to(device)
 
-            # compute output
-            output = model(inputs)
-            loss = criterion(output, targets)
+      # compute output
+      output = model(inputs)
+      loss = criterion(output, targets)
 
-            # measure accuracy and record loss
-            prec1, prec5 = accuracy(output, targets, topk=(1, 2))
-            losses.update(loss.item(), inputs.size(0))
-            top1.update(prec1, inputs.size(0))
-            top5.update(prec5, inputs.size(0))
+      # measure accuracy and record loss
+      prec1, prec5 = accuracy(output, targets, topk=(1, 2))
+      losses.update(loss.item(), inputs.size(0))
+      top1.update(prec1, inputs.size(0))
+      top5.update(prec5, inputs.size(0))
 
-            # compute gradients in a backward pass
-            optimizer.zero_grad()
-            loss.backward()
+      # compute gradients in a backward pass
+      optimizer.zero_grad()
+      loss.backward()
 
-            # Call step of optimizer to update model params
-            optimizer.step()
+      # Call step of optimizer to update model params
+      optimizer.step()
 
-            print(f"Epoch [{epoch + 1}] [{i + 1}/{len(train_dataloader)}]\t"
-                  f"Loss {loss.item():.4f}\t"
-                  f"Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t"
-                  f"Prec@5 {top5.val:.3f} ({top5.avg:.3f})", end="\r")
+      print(f"Epoch [{epoch + 1}] [{i + 1}/{len(train_dataloader)}]\t"
+            f"Loss {loss.item():.4f}\t"
+            f"Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t"
+            f"Prec@5 {top5.val:.3f} ({top5.avg:.3f})", end="\r")
 
-        # save model file
-        torch.save(CNN.state_dict(), MODEL_PATH)
+    # save model file
+    torch.save(CNN.state_dict(), MODEL_PATH)
 
 
 def test():
-    CNN = AlexNet(num_classes=opt.num_classes)
-    CNN.load_state_dict(torch.load(MODEL_PATH, map_location=lambda storage, loc: storage))
-    CNN.to(device)
-    CNN.eval()
+  if torch.cuda.device_count() > 1:
+    model = torch.nn.parallel.DataParallel(AlexNet(num_classes=opt.num_classes))
+  else:
+    model = AlexNet(num_classes=opt.num_classes)
+  model.load_state_dict(torch.load(MODEL_PATH, map_location=lambda storage, loc: storage))
+  model.to(device)
 
-    # init value
-    total = 0.
-    correct = 0.
-    with torch.no_grad():
-        for i, data in enumerate(test_dataloader):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, targets = data
-            inputs = inputs.to(device)
-            targets = targets.to(device)
+  # init value
+  total = 0.
+  correct = 0.
+  with torch.no_grad():
+    for i, data in enumerate(test_dataloader):
+      # get the inputs; data is a list of [inputs, labels]
+      inputs, targets = data
+      inputs = inputs.to(device)
+      targets = targets.to(device)
 
-            outputs = CNN(inputs)
-            _, predicted = torch.max(outputs.data, 1)
-            total += targets.size(0)
-            correct += (predicted == targets).sum().item()
+      outputs = CNN(inputs)
+      _, predicted = torch.max(outputs.data, 1)
+      total += targets.size(0)
+      correct += (predicted == targets).sum().item()
 
-    acc = 100 * correct / total
-    return acc
+  acc = 100 * correct / total
+  return acc
 
 
 if __name__ == '__main__':
-    if opt.name is None:
-        print(parser.print_help())
-        exit(0)
-    elif opt.phase == "train":
-        train()
-    elif opt.phase == "eval":
-        print("Loading model successful!")
-        accuracy = test()
-        print(
-            f"\nAccuracy of the network on the test images: {accuracy:.2f}%.\n")
+  if opt.name is None:
+    print(parser.print_help())
+    exit(0)
+  elif opt.phase == "train":
+    train()
+  elif opt.phase == "eval":
+    print("Loading model successful!")
+    accuracy = test()
+    print(
+      f"\nAccuracy of the network on the test images: {accuracy:.2f}%.\n")
