261af7befc7d87876356cba94e4d1eccdd3c9e34
Changyu Liu <liuchangyu1111@gmail.com>
2019-12-20 10:34:00 +0800

    update network arch

--- example/cifar.py
+++ example/cifar.py
@@ -132,7 +132,7 @@ class AlexNet(nn.Module):
       nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
       nn.ReLU(inplace=True),
       nn.MaxPool2d(kernel_size=3, stride=2),
-      nn.Conv2d(64, 192, kernel_size=5, padding=2),
+      nn.Conv2d(64, 192, kernel_size=3, padding=1),
       nn.ReLU(inplace=True),
       nn.MaxPool2d(kernel_size=3, stride=2),
       nn.Conv2d(192, 384, kernel_size=3, padding=1),
@@ -141,16 +141,15 @@ class AlexNet(nn.Module):
       nn.ReLU(inplace=True),
       nn.Conv2d(256, 256, kernel_size=3, padding=1),
       nn.ReLU(inplace=True),
-      nn.MaxPool2d(kernel_size=3, stride=2),
     )
     self.classifier = nn.Sequential(
       nn.Dropout(),
-      nn.Linear(256 * 1 * 1, 500),
+      nn.Linear(256 * 3 * 3, 4096),
       nn.ReLU(inplace=True),
       nn.Dropout(),
-      nn.Linear(500, 500),
+      nn.Linear(4096, 4096),
       nn.ReLU(inplace=True),
-      nn.Linear(500, num_classes),
+      nn.Linear(4096, num_classes),
     )
 
   def forward(self, x):
