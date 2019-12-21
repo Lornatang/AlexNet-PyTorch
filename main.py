3e1683a2d95c9405e53e7b924f1d7037212ea9e2
Changyu Liu <liuchangyu1111@gmail.com>
2019-12-21 09:13:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ parser.add_argument('--img_size', type=int, default=224,
                     help='the height / width of the inputs image to network')
 parser.add_argument('--num_classes', type=int, default=1000,
                     help="number of dataset category.")
-parser.add_argument('--lr', type=float, default=0.0001,
-                    help="learning rate. Default: 0.0001. For use adjust learning rate.")
+parser.add_argument('--lr', type=float, default=0.01,
+                    help="learning rate. Default: 0.01. For use adjust learning rate.")
 parser.add_argument('--epochs', type=int, default=500, help="Train loop")
 parser.add_argument('--phase', type=str, default='eval',
                     help="train or eval? default:`eval`")
