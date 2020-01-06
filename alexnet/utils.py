c20029f0e9c0845bed172381b2190a9954a2a0fc
Changyu Liu <liuchangyu1111@gmail.com>
2020-01-06 22:41:18 +0800

    Update utils.py

--- alexnet/utils.py
+++ alexnet/utils.py
@@ -12,8 +12,6 @@
 # limitations under the License.
 # ==============================================================================
 
-
-"""Dynamic adjustment of parameters tool implementation"""
 def adjust_learning_rate(optimizer, epoch, args):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
     lr = args.lr * (0.1 ** (epoch // 30))
