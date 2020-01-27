a897cefedf3376575cd9b65dea1b41801d4654fb
Changyu Liu <liuchangyu1111@gmail.com>
2020-01-27 19:32:07 +0800

    Update test.py

--- examples/simple/test.py
+++ examples/simple/test.py
@@ -24,32 +24,38 @@ from PIL import Image
 
 from alexnet import AlexNet
 
-image_size = 224
-
 # Open image
-img = Image.open('panda.jpg')
+input_image = Image.open("img.jpg")
 
 # Preprocess image
-tfms = transforms.Compose([transforms.Resize(image_size), transforms.CenterCrop(image_size),
-                           transforms.ToTensor(),
-                           transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]), ])
-img = tfms(img).unsqueeze(0)
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+input_tensor = preprocess(input_image)
+input_batch = input_tensor.unsqueeze(0)  # create a mini-batch as expected by the model
 
 # Load class names
-labels_map = json.load(open('labels_map.txt'))
+labels_map = json.load(open("labels_map.txt"))
 labels_map = [labels_map[str(i)] for i in range(1000)]
 
 # Classify with AlexNet
-print("=> loading checkpoint 'alexnet'.")
-model = AlexNet.from_pretrained('alexnet')
-print("=> loaded checkpoint 'alexnet'.")
+model = AlexNet.from_pretrained("alexnet")
 model.eval()
+
+# move the input and model to GPU for speed if available
+if torch.cuda.is_available():
+    input_batch = input_batch.to("cuda")
+    model.to("cuda")
+
 with torch.no_grad():
-    logits = model(img)
+    logits = model(input_batch)
 preds = torch.topk(logits, k=5).indices.squeeze(0).tolist()
 
-print('-----')
+print("-----")
 for idx in preds:
     label = labels_map[idx]
     prob = torch.softmax(logits, dim=1)[0, idx].item()
-    print('{:<75} ({:.2f}%)'.format(label, prob * 100))
+    print(f"{label:<75} ({prob * 100:.2f}%)")
