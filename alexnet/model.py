38109f4e1a39b43c7cd23c1e96929f89b92e1a36
trainbot <liuchangyu98@outlook.com>
2020-02-12 16:42:18 +0800

    Remove redundant variables and install packages

--- alexnet/model.py
+++ alexnet/model.py
@@ -23,79 +23,78 @@ from .utils import load_pretrained_weights
 # AlexNet model architecture from the One weird trick...
 # <https://arxiv.org/abs/1404.5997>`_ paper.
 class AlexNet(nn.Module):
-    """ An AlexNet model. Most easily loaded with the .from_name or .from_pretrained methods
+  """ An AlexNet model. Most easily loaded with the .from_name or
+  .from_pretrained methods
 
-    Args:
-      global_params (namedtuple): A set of GlobalParams shared between blocks
+  Args:
+    global_params (namedtuple): A set of GlobalParams shared between blocks
 
-    Example:
-        model = AlexNet.from_pretrained("alexnet")
-    """
+  Example:
+      model = AlexNet.from_pretrained("alexnet")
+  """
 
-    def __init__(self, global_params=None):
-        super().__init__()
-        self._global_params = global_params
+  def __init__(self, global_params=None):
+    super().__init__()
+    self._global_params = global_params
 
-        avg_size = self._global_params.avg_size
-        dropout_rate = self._global_params.dropout_rate
-        image_size = self._global_params.image_size
-        num_classes = self._global_params.num_classes
+    dropout_rate = self._global_params.dropout_rate
+    num_classes = self._global_params.num_classes
 
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3,stride=2),
-            nn.Conv2d(64,192,kernel_size=5,padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3,stride=2),
-            nn.Conv2d(192,384,kernel_size=3,padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384,256,kernel_size=3,padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256,256,kernel_size=3,padding=1),
-            nn.ReLU(inplace=True),  
-            nn.MaxPool2d(kernel_size=3,stride=2)
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.classifier = nn.Sequential(
-            nn.Dropout(p=dropout_rate),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=dropout_rate),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
+    self.features = nn.Sequential(
+      nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(kernel_size=3, stride=2),
+      nn.Conv2d(64, 192, kernel_size=5, padding=2),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(kernel_size=3, stride=2),
+      nn.Conv2d(192, 384, kernel_size=3, padding=1),
+      nn.ReLU(inplace=True),
+      nn.Conv2d(384, 256, kernel_size=3, padding=1),
+      nn.ReLU(inplace=True),
+      nn.Conv2d(256, 256, kernel_size=3, padding=1),
+      nn.ReLU(inplace=True),
+      nn.MaxPool2d(kernel_size=3, stride=2)
+    )
+    self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+    self.classifier = nn.Sequential(
+      nn.Dropout(p=dropout_rate),
+      nn.Linear(256 * 6 * 6, 4096),
+      nn.ReLU(inplace=True),
+      nn.Dropout(p=dropout_rate),
+      nn.Linear(4096, 4096),
+      nn.ReLU(inplace=True),
+      nn.Linear(4096, num_classes),
+    )
 
-    def forward(self, x):
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
+  def forward(self, x):
+    x = self.features(x)
+    x = self.avgpool(x)
+    x = torch.flatten(x, 1)
+    x = self.classifier(x)
+    return x
 
-    @classmethod
-    def from_name(cls, model_name, override_params=None):
-        cls._check_model_name_is_valid(model_name)
-        global_params = get_model_params(model_name, override_params)
-        return cls(global_params)
+  @classmethod
+  def from_name(cls, model_name, override_params=None):
+    cls._check_model_name_is_valid(model_name)
+    global_params = get_model_params(model_name, override_params)
+    return cls(global_params)
 
-    @classmethod
-    def from_pretrained(cls, model_name, num_classes=1000):
-        model = cls.from_name(model_name, override_params={"num_classes": num_classes})
-        load_pretrained_weights(model, model_name, load_fc=(num_classes == 1000)
-        return model
+  @classmethod
+  def from_pretrained(cls, model_name, num_classes=1000):
+    model = cls.from_name(model_name, override_params={"num_classes": num_classes})
+    load_pretrained_weights(model, model_name, load_fc=(num_classes == 1000))
+    return model
 
-    @classmethod
-    def get_image_size(cls, model_name):
-        cls._check_model_name_is_valid(model_name)
-        _, res = alexnet_params(model_name)
-        return res
+  @classmethod
+  def get_image_size(cls, model_name):
+    cls._check_model_name_is_valid(model_name)
+    _, res = alexnet_params(model_name)
+    return res
 
-    @classmethod
-    def _check_model_name_is_valid(cls, model_name):
-        """ Validates model name. None that pretrained weights are only available for
-        the first four models (alexnet) at the moment. """
-        valid_model = "alexnet"
-        if model_name not in valid_model:
-            raise ValueError("model_name should be one of: " + ", ".join(valid_models))
+  @classmethod
+  def _check_model_name_is_valid(cls, model_name):
+    """ Validates model name. None that pretrained weights are only available for
+    the first four models (alexnet) at the moment. """
+    valid_model = "alexnet"
+    if model_name not in valid_model:
+      raise ValueError("model_name should be one of: " + ", ".join(valid_model))
