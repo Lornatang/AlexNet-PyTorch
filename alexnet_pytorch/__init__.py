028d3cbf1e59d97acdb7afac13c118fcddfade85
LiuChangyu <liuchangyu1111@gmail.com>
2020-02-16 15:13:12 +0800

    Update the codebase version number

--- alexnet_pytorch/__init__.py
+++ alexnet_pytorch/__init__.py
@@ -12,6 +12,6 @@
 # limitations under the License.
 # ==============================================================================
 
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 from .model import AlexNet
