0b35cf4a6be078b61a609b83d0eb059fd7b49681
LiuChangyu <liuchangyu1111@gmail.com>
2020-02-16 15:12:34 +0800

    Initialization weight
    The neural network layer was initialized by using the initialization weight method in ResNet paper

--- alexnet_pytorch/model.py
+++ alexnet_pytorch/model.py
@@ -66,6 +66,18 @@ class AlexNet(nn.Module):
       nn.Linear(4096, num_classes),
     )
 
+    for m in self.modules():
+      if isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        if m.bias is not None:
+          nn.init.constant_(m.bias, 0)
+      elif isinstance(m, nn.BatchNorm2d):
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
+      elif isinstance(m, nn.Linear):
+        nn.init.normal_(m.weight, 0, 0.01)
+        nn.init.constant_(m.bias, 0)
+
   def extract_features(self, inputs):
     """ Returns output of the final convolution layer """
     x = self.features(inputs)
