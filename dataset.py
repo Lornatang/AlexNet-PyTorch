9840e70a67e5421a729464e6a2e27aab5799e00c
Lorna <liuchangyu1111@gmail.com>
2022-08-03 10:40:03 +0800

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,213 @@
+# Copyright 2022 Dakewe Biotech Corporation. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import queue
+import sys
+import threading
+from glob import glob
+
+import cv2
+import torch
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.datasets.folder import find_classes
+from torchvision.transforms import TrivialAugmentWide, RandomErasing
+
+import imgproc
+
+__all__ = [
+    "ImageDataset",
+    "PrefetchGenerator", "PrefetchDataLoader", "CPUPrefetcher", "CUDAPrefetcher",
+]
+
+# Image formats supported by the image processing library
+IMG_EXTENSIONS = ("jpg", "jpeg", "png", "ppm", "bmp", "pgm", "tif", "tiff", "webp")
+
+# The delimiter is not the same between different platforms
+if sys.platform == "win32":
+    delimiter = "\\"
+else:
+    delimiter = "/"
+
+
+class ImageDataset(Dataset):
+    """Define training/valid dataset loading methods.
+
+    Args:
+        image_dir (str): Train/Valid dataset address.
+        image_size (int): Image size.
+        mode (str): Data set loading method, the training data set is for data enhancement, and the verification data set is not for data enhancement.
+    """
+
+    def __init__(self, image_dir: str, image_size: int, mode: str) -> None:
+        super(ImageDataset, self).__init__()
+        # Iterate over all image paths
+        self.image_file_paths = glob(f"{image_dir}/*/*")
+        # Form image class label pairs by the folder where the image is located
+        _, self.class_to_idx = find_classes(image_dir)
+        self.image_size = image_size
+        self.mode = mode
+        self.delimiter = delimiter
+
+        if self.mode == "Train":
+            # Use PyTorch's own data enhancement to enlarge and enhance data
+            self.data_augmentation = transforms.Compose([
+                transforms.RandomResizedCrop([self.image_size, self.image_size]),
+                # TrivialAugmentWide(),
+                # RandomErasing(0.1),
+                transforms.RandomRotation([0, 270]),
+                transforms.RandomHorizontalFlip(0.5),
+                transforms.RandomVerticalFlip(0.5),
+            ])
+
+    def __getitem__(self, batch_index: int) -> [torch.Tensor, int]:
+        # Read a batch of image data
+        if self.image_file_paths[batch_index].split(self.delimiter)[-1].split(".")[-1].lower() in IMG_EXTENSIONS:
+            image = cv2.imread(self.image_file_paths[batch_index])
+            target = self.class_to_idx[self.image_file_paths[batch_index].split(self.delimiter)[-2]]
+        else:
+            raise ValueError(f"Unsupported image extensions, Only support `{IMG_EXTENSIONS}`, "
+                             "please check the image file extensions.")
+
+        # BGR to RGB
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # OpenCV convert PIL
+        image = Image.fromarray(image)
+
+        if self.mode == "Train":
+            # Data augmentation
+            image = self.data_augmentation(image)
+
+        # Convert image data into Tensor stream format (PyTorch).
+        # Note: The range of input and output is between [0, 1]
+        tensor = imgproc.image_to_tensor(image, False, False)
+
+        return {"image": tensor, "target": target}
+
+    def __len__(self) -> int:
+        return len(self.image_file_paths)
+
+
+class PrefetchGenerator(threading.Thread):
+    """A fast data prefetch generator.
+
+    Args:
+        generator: Data generator.
+        num_data_prefetch_queue (int): How many early data load queues.
+    """
+
+    def __init__(self, generator, num_data_prefetch_queue: int) -> None:
+        threading.Thread.__init__(self)
+        self.queue = queue.Queue(num_data_prefetch_queue)
+        self.generator = generator
+        self.daemon = True
+        self.start()
+
+    def run(self) -> None:
+        for item in self.generator:
+            self.queue.put(item)
+        self.queue.put(None)
+
+    def __next__(self):
+        next_item = self.queue.get()
+        if next_item is None:
+            raise StopIteration
+        return next_item
+
+    def __iter__(self):
+        return self
+
+
+class PrefetchDataLoader(DataLoader):
+    """A fast data prefetch dataloader.
+
+    Args:
+        num_data_prefetch_queue (int): How many early data load queues.
+        kwargs (dict): Other extended parameters.
+    """
+
+    def __init__(self, num_data_prefetch_queue: int, **kwargs) -> None:
+        self.num_data_prefetch_queue = num_data_prefetch_queue
+        super(PrefetchDataLoader, self).__init__(**kwargs)
+
+    def __iter__(self):
+        return PrefetchGenerator(super().__iter__(), self.num_data_prefetch_queue)
+
+
+class CPUPrefetcher:
+    """Use the CPU side to accelerate data reading.
+
+    Args:
+        dataloader (DataLoader): Data loader. Combines a dataset and a sampler, and provides an iterable over the given dataset.
+    """
+
+    def __init__(self, dataloader) -> None:
+        self.original_dataloader = dataloader
+        self.data = iter(dataloader)
+
+    def next(self):
+        try:
+            return next(self.data)
+        except StopIteration:
+            return None
+
+    def reset(self):
+        self.data = iter(self.original_dataloader)
+
+    def __len__(self) -> int:
+        return len(self.original_dataloader)
+
+
+class CUDAPrefetcher:
+    """Use the CUDA side to accelerate data reading.
+
+    Args:
+        dataloader (DataLoader): Data loader. Combines a dataset and a sampler, and provides an iterable over the given dataset.
+        device (torch.device): Specify running device.
+    """
+
+    def __init__(self, dataloader, device: torch.device):
+        self.batch_data = None
+        self.original_dataloader = dataloader
+        self.device = device
+
+        self.data = iter(dataloader)
+        self.stream = torch.cuda.Stream()
+        self.preload()
+
+    def preload(self):
+        try:
+            self.batch_data = next(self.data)
+        except StopIteration:
+            self.batch_data = None
+            return None
+
+        with torch.cuda.stream(self.stream):
+            for k, v in self.batch_data.items():
+                if torch.is_tensor(v):
+                    self.batch_data[k] = self.batch_data[k].to(self.device, non_blocking=True)
+
+    def next(self):
+        torch.cuda.current_stream().wait_stream(self.stream)
+        batch_data = self.batch_data
+        self.preload()
+        return batch_data
+
+    def reset(self):
+        self.data = iter(self.original_dataloader)
+        self.preload()
+
+    def __len__(self) -> int:
+        return len(self.original_dataloader)
