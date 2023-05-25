ee1dfb6886a7b4893dce09112b71737a3b5abfe5
AlexUrs99 <56313283+AlexUrs99@users.noreply.github.com>
2023-05-25 12:13:43 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -23,33 +23,33 @@ __all__ = [
 ]
 
 
-def accuracy(output, target, topk=(1,)): ## undefined
+def accuracy(output, target, topk=(1,)): ## Utility function to compare the output with the target (accuracy) of top k predictions (defaulted to 1)
     """Computes the accuracy over the k top predictions for the specified values of k"""
-    with torch.no_grad(): ## undefined
-        maxk = max(topk) ## undefined
-        batch_size = target.size(0) ## undefined
+    with torch.no_grad(): ## No gradient calculations should be performed (performance boost when only evaluation is needed) 
+        maxk = max(topk) ## Max of topk
+        batch_size = target.size(0) ## Chooses target's tensor first dimension as batch size
 
-        _, pred = output.topk(maxk, 1, True, True) ## undefined
-        pred = pred.t() ## undefined
+        _, pred = output.topk(maxk, 1, True, True) ## Retrieve top k predicitons of model's output along with their indices. '_' discards the values, indices are stored in 'pred'
+        pred = pred.t() ## Transposes the pred tensor
         correct = pred.eq(target.view(1, -1).expand_as(pred)) ## undefined
 
-        results = [] ## undefined
-        for k in topk: ## undefined
-            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True) ## undefined
-            results.append(correct_k.mul_(100.0 / batch_size)) ## undefined
-        return results ## undefined
+        results = [] ## Declares an empty array where accuracies will be stored
+        for k in topk: ## Iterate over all values of top k predictions
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True) ## Selects the first k rows from the 'correct' tensor and reshapes it into a vector. It then calculates the sum of all the values
+            results.append(correct_k.mul_(100.0 / batch_size)) ## The correct predictions of k are averaged by the batch size and then added to the resulted accuracies list
+        return results ## Return the resulted accuracies
 
 
-def load_state_dict( ## undefined
-        model: nn.Module, ## undefined
-        model_weights_path: str, ## undefined
-        ema_model: nn.Module = None, ## undefined
-        start_epoch: int = None, ## undefined
-        best_acc1: float = None, ## undefined
-        optimizer: torch.optim.Optimizer = None, ## undefined
-        scheduler: torch.optim.lr_scheduler = None, ## undefined
+def load_state_dict( ## Defines a method named load_state_dict
+        model: nn.Module, ## The model on which the loaded weights will be applied
+        model_weights_path: str, ## The path to the file containing the saved model weights and parameters
+        ema_model: nn.Module = None, ## Optional for Exponential Moving Average model, defaulted to None
+        start_epoch: int = None, ## Optional regarding the epoch on which to start the training process, defaulted to None
+        best_acc1: float = None, ## Optional float to describe the best accuracy achieved during training defaulted to None
+        optimizer: torch.optim.Optimizer = None, ## Optional optimizer object defaulted to None
+        scheduler: torch.optim.lr_scheduler = None, ## Optional learning rate scheduler object defaulted to None
         load_mode: str = None,
-) -> [nn.Module, nn.Module, str, int, float, torch.optim.Optimizer, torch.optim.lr_scheduler]: ## undefined
+) -> [nn.Module, nn.Module, str, int, float, torch.optim.Optimizer, torch.optim.lr_scheduler]: ## The return type of the method
     # Load model weights
     checkpoint = torch.load(model_weights_path, map_location=lambda storage, loc: storage) ## undefined
 
@@ -114,24 +114,24 @@ class Summary(Enum):
     COUNT = 3
 
 
-class AverageMeter(object): ## undefined
-    def __init__(self, name, fmt=":f", summary_type=Summary.AVERAGE): ## undefined
-        self.name = name ## undefined
-        self.fmt = fmt ## undefined
-        self.summary_type = summary_type ## undefined
-        self.reset() ## undefined
+class AverageMeter(object): ##Utility class for computing the average of a value over multiple iterations  
+    def __init__(self, name, fmt=":f", summary_type=Summary.AVERAGE): ## Object constructor with a lavbel for the meter, formating as defaulted to floating point number and the summary type
+        self.name = name ## Initializes the meter label
+        self.fmt = fmt ## Initializes the format type
+        self.summary_type = summary_type ## Initializes the summary type
+        self.reset() ## Private method to initialize object parameters with default values
 
-    def reset(self): ## undefined
+    def reset(self): ## Private method to reset the state of the average object
         self.val = 0
         self.avg = 0
         self.sum = 0
         self.count = 0
 
-    def update(self, val, n=1): ## undefined
-        self.val = val ## undefined
-        self.sum += val * n ## undefined
-        self.count += n ## undefined
-        self.avg = self.sum / self.count ## undefined
+    def update(self, val, n=1): ## Method used to update the values the object during iterations
+        self.val = val ## Update value with new value
+        self.sum += val * n ## Update the sum with the value * number of iterations
+        self.count += n ## Add to the number of iterations
+        self.avg = self.sum / self.count ## Average of the sum based on number of iterations
 
     def __str__(self):
         fmtstr = "{name} {val" + self.fmt + "} ({avg" + self.fmt + "})"
@@ -152,23 +152,23 @@ class AverageMeter(object): ## undefined
         return fmtstr.format(**self.__dict__)
 
 
-class ProgressMeter(object): ## undefined
-    def __init__(self, num_batches, meters, prefix=""): ## undefined
-        self.batch_fmtstr = self._get_batch_fmtstr(num_batches) ## undefined
+class ProgressMeter(object): ## Declaration of a utlity class for displaying information about training and evaluation process
+    def __init__(self, num_batches, meters, prefix=""): ## Object constructor
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches) ## Total number of batches in the training or evaluation process
         self.meters = meters
         self.prefix = prefix
 
-    def display(self, batch): ## undefined
+    def display(self, batch): ## Prints progress for current batch
         entries = [self.prefix + self.batch_fmtstr.format(batch)] ## undefined
         entries += [str(meter) for meter in self.meters] ## undefined
         print("\t".join(entries))
 
-    def display_summary(self): ## undefined
-        entries = [" *"] ## undefined
-        entries += [meter.summary() for meter in self.meters] ## undefined
-        print(" ".join(entries)) ## undefined
+    def display_summary(self): ## Method to display the summary of the meters
+        entries = [" *"] ## Initializes the entries array
+        entries += [meter.summary() for meter in self.meters] ## Concatenates the summary of each meter 
+        print(" ".join(entries)) ## Joins the results by empty string and displays the resulted string
 
-    def _get_batch_fmtstr(self, num_batches): ## undefined
-        num_digits = len(str(num_batches // 1)) ## undefined
+    def _get_batch_fmtstr(self, num_batches): ## Private method used in constructor to format a string displaying batch count / total number of batches
+        num_digits = len(str(num_batches // 1)) ## Integer divison for num_batches to work for the lengthv of the string
         fmt = "{:" + str(num_digits) + "d}" ## undefined
-        return "[" + fmt + "/" + fmt.format(num_batches) + "]" ## undefined
+        return "[" + fmt + "/" + fmt.format(num_batches) + "]" ## Formats string with aggregated data 
